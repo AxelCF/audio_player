e03e85ebe7a20e92cda35f5d7f9a4de50b578522
AxelCF <axelmaurice.1997@gmail.com>
2022-02-06 15:55:15 +0100

    index

--- index.php
+++ index.php
@@ -1,12 +1,65 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
+        <link rel="stylesheet" href="./ASSETS/CSS/body.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins&family=Prompt&display=swap" rel="stylesheet">
+        <title>Audio player</title>
         <script src="./ASSETS/JS/scripts.js" defer></script>
     </head>
     <body>
-        
+        <h2>Pour vous</h2>
+        <div class="container1">
+
+            <div class="text">
+            <div class="case">
+
+            </div>
+            <p>Un mix pas infini parce que faut pas déconner</p>
+            </div>
+
+            <div class="text">
+            <div class="case">
+
+            </div>
+            <p>Un mix pas infini parce que faut pas déconner</p>
+            </div>
+
+            <div class="text">
+            <div class="case">
+
+
+            </div>
+            <p>Un mix pas infini parce que faut pas déconner</p>
+            </div>
+
+            <div class="text">
+            <div class="case">
+
+            </div>
+            <p>Un mix pas infini parce que faut pas déconner</p>
+            </div>
+        </div>
+
+
+        <h2>Découverte</h2>
+        <div class="container2">
+
+            <div class="case">
+
+            </div>
+            <div class="case">
+
+            </div>
+            <div class="case">
+
+            </div>
+            <div class="case">
+
+            </div>
+        </div>
     </body>
 </html>
