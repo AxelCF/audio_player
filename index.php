83d5a7c3b1c74cb12284b841db3b15542e76e134
Dylan D'antonio <dyland.antonio.dev42@gmail.com>
2022-02-07 11:16:55 +0100

    maj

--- index.php
+++ index.php
@@ -3,10 +3,51 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
+        <title>Audio player</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins&family=Prompt&display=swap" rel="stylesheet">
         <script src="./ASSETS/JS/scripts.js" defer></script>
+        <link rel="stylesheet" href="./ASSETS/CSS/body.css">
     </head>
     <body>
+    <h2>Pour vous</h2>
+        <div class="container1">
+            <div class="text">
+                <div class="case">
         
+                </div>
+                <p>Un mix pas infini parce que il ne faut pas déconner</p>
+            </div>
+            <div class="text">
+                <div class="case">
+
+                </div>
+                <p>Un mix pas infini parce que il ne faut pas déconner</p>
+            </div>
+            <div class="text">
+
+                <div class="case">
+                </div>
+                <p>Un mix pas infini parce que il ne faut pas déconner</p>
+            </div>
+            <div class="text">
+                
+                <div class="case">
+                </div>
+                <p>Un mix pas infini parce que il ne faut pas déconner</p>
+            </div>
+        </div>
+        <h2>Découverte</h2>
+        <div class="container2">
+            <div class="case">
+                </div>
+                <div class="case">
+                </div>
+                <div class="case">
+                </div>
+                <div class="case">
+            </div>
+        </div>
     </body>
 </html>
