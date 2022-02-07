1bf9dbbcaf8b1bfa2ef803a6cabd3ea48057b8e6
Dylan D'antonio <dyland.antonio.dev42@gmail.com>
2022-02-07 14:00:28 +0100

    form accepted ?

---
+++ form.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <? include("/laragon/www/audio_player/INC/head.php") ?>
+    <body>
+        <? include("/laragon/www/audio_player/INC/header.php") ?>
+        <form action="" method="post">
+            <label for="">Adresse du fichier</label>
+            <input type="text" name="linkFile">
+            <br>
+            <br>
+            <label for="">Adresse de l'image</label>
+            <input type="text" name="linkCapture">
+            <input type="submit" name="submit">
+        </form>
+        <? include("/laragon/www/audio_player/INC/footer.php") ?>
+    </body>
+</html>
