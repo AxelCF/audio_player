76b4901dd1499c1de9587dd8257a1765179e489d
AxelCF <axelmaurice1997@gmail.com>
2022-03-23 09:52:27 +0100

    def

--- ASSETS/JS/scripts_playlist.js
+++ ASSETS/JS/scripts_playlist.js
@@ -1,19 +1,25 @@
-fetch('./ASSETS/PHP/PROCESS/recup_playlist.php').then(function(response) {
-    return response.json();
-}).then(function(text){
-console.log(text);
 
-    
- text.forEach(cover => {
-     document.querySelector('.playlists').innerHTML += `
 
-    <div class="text" id="playlist-${cover.id} data-id="${cover.id}">
+getPlaylists()
+
+function getPlaylists() {
+    fetch('./ASSETS/PHP/PROCESS/recup_playlist.php').then(function (response) {
+        return response.json();
+    }).then(function (text) {
+        console.log(text);
+
+
+        text.forEach(cover => {
+            document.querySelector('.playlists').innerHTML += `
+
+    <div class="text" id="playlist-${cover.id}" data-id="${cover.id}">
     <div class="case">
         <img src="./ASSETS/COVER/${cover.image}">
     </div>
         <p>Un mix pas infini parce que il ne faut pas déconner</p>
-    </div>`
+    </div>`;
 
 
-});
-})
+        });
+    });
+}
--- ASSETS/JS/scripts_song.js
+++ ASSETS/JS/scripts_song.js
@@ -1,14 +1,50 @@
-fetch('./ASSETS/PHP/PROCESS/recup_song.php').then(function(response) {
+
+
+fetch('./ASSETS/PHP/PROCESS/recup_playlist.php').then(function (response) {
     return response.json();
-}).then(function(text){
-console.log(text);
+}).then(function (text) {
+    console.log(text);
 
-    let songLinks = []
+
+    text.forEach(cover => {
+        document.querySelector('.playlists').innerHTML += `
+
+<div class="text" id="playlist-${cover.id}" data-id="${cover.id}">
+<div class="case">
+    <img src="./ASSETS/COVER/${cover.image}">
+</div>
+    <p>Un mix pas infini parce que il ne faut pas déconner</p>
+</div>`;
+
+
+    });
+}).then(_ => {
+    const idPlaylists = document.querySelectorAll('.text');
+    idPlaylists.forEach((element) =>{
+        const idPlaylist = element.dataset.id;
+        
+        element.addEventListener('click', () =>{
+            getSongsFromPlaylist(idPlaylist);
     
- text.forEach(song => {
-     console.log(song.link);
-     songLinks.push('./ASSETS/MUSIC/' + song.link)
-
-});
-arrayLinks = songLinks
+        })
+    })
+    
+    getSongsFromPlaylist();
 })
+
+function getSongsFromPlaylist(id) {
+    fetch('./ASSETS/PHP/PROCESS/recup_song.php).then(function (response) {
+        return response.text();
+    }).then(function (text) {
+        console.log(text);
+
+        let songLinks = [];
+
+        text.forEach(song => {
+            console.log(song.link);
+            songLinks.push('./ASSETS/MUSIC/' + song.link);
+
+        });
+        arrayLinks = songLinks;
+    });
+}
